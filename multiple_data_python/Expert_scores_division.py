4c6fbde95a8bbe358a02ee1d04c4450d879cd642
Anne-lauretho <anne-laure.thomas@etu.umontpellier.fr>
2025-05-01 19:51:13 +0200

    Change of division code and report

--- multiple_data_python/Expert_scores_division.py
+++ multiple_data_python/Expert_scores_division.py
@@ -1,24 +1,29 @@
 # Division des données expertes 
-#%%
+# %%
 import json
+import random
 
-# Charger le JSON
-with open("expert_processed_scores.json", "r") as file:
-    expert_data = json.load(file)
+# Charger les données
+with open("expert_processed_scores.json", "r") as f:
+    expert_data = json.load(f)
 
-# Obtenir les clés et diviser en deux
+# Rendre le tirage reproductible
+random.seed(42)
+
+# Mélanger les clés
 keys = list(expert_data.keys())
+random.shuffle(keys)
+
+# Diviser en deux moitiés
 mid = len(keys) // 2
+expert_half1 = {k: expert_data[k] for k in keys[:mid]}
+expert_half2 = {k: expert_data[k] for k in keys[mid:]}
 
-expert_half1 = {key: expert_data[key] for key in keys[:mid]}
-expert_half2 = {key: expert_data[key] for key in keys[mid:]}
+# Sauvegarder les moitiés
+with open("expert_scores1.json", "w") as f:
+    json.dump(expert_half1, f, indent=4)
 
-# Sauvegarder les fichiers
-with open("expert_scores1.json", "w") as file:
-    json.dump(expert_half1, file, indent=4)
-
-with open("expert_scores2.json", "w") as file:
-    json.dump(expert_half2, file, indent=4)
-
+with open("expert_scores2.json", "w") as f:
+    json.dump(expert_half2, f, indent=4)
 
 # %%
